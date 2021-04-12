ad6d1f82348e168ef3acf7d799cb90ea8326c733
dimo12317 <79600828+dimo12317@users.noreply.github.com>
2021-04-13 02:46:15 +0300

    Update 11-encode-line.js

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,36 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const a = str.split('');
+  let b = a[0];
+  let kol = 0;
+  let strin = '';
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] === b) {
+      kol++;
+    } else {
+      strin += kol;
+      strin += b;
+      b = a[i];
+      kol = 1;
+    }
+    if (i === a.length - 1) {
+      strin += kol;
+      strin += b;
+      b = a[i];
+      kol = 1;
+    }
+  }
+  strin = strin.split('');
+  for (let i = 0; i < strin.length; i++) {
+    if (strin[i] === '1') {
+      strin.splice(i, 1);
+      i--;
+    }
+  }
+  strin = strin.join('');
+  return strin;
 }
 
 module.exports = encodeLine;
